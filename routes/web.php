f0131cf23288cc5a66cf07d58e5acbb0af19ee2d
relaxsbk.exe <pah4n1989@gmail.com>
2024-04-22 03:58:53 +0800

    чат готов

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::redirect('/', '/chatify');
+Route::redirect('/home', '/chatify');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
